996240361d925199db1b6ea10856ee5fb81ac3c8
faruk <faruk_yildiz@protonmail.com>
2025-07-31 22:05:52 +0300

    Implement environmental variables, start on fetching and parsing data

--- Weather-Service/WeatherApi.cpp
+++ Weather-Service/WeatherApi.cpp
@@ -1,10 +1,21 @@
 #include "WeatherApi.h"
 
+const QString ApiAccess::getApiKey()
+{
+    return QProcessEnvironment::systemEnvironment()
+        .value("API_KEY", "");
+}
+const QString ApiAccess::getApiCityCountry()
+{
+    return QProcessEnvironment::systemEnvironment()
+    .value("API_CITY_COUNTRY", "");
+}
+
 WeatherApi::WeatherApi(QObject *parent)
     : QObject{parent}
 {
     m_info = "-C";
-    m_Manager = new QNetworkAccessManager(this);
+    m_manager = new QNetworkAccessManager(this);
 }
 
 QString WeatherApi::info() const
@@ -14,17 +25,45 @@ QString WeatherApi::info() const
 
 void WeatherApi::fetchData()
 {
-    QString apiKey= "";
-    QString city = "Athens";
-    QString countryCode = "GRC"; //ISO 3166 code
+    if(ApiAccess::getApiKey().isEmpty()) {
+        qWarning() << "Environmental variable \"API_KEY\" is empty!";
+        return;
+    }
 
-    // API request for longitude and latitude
+    if(m_reply) {
+        m_reply->abort();
+        m_reply->deleteLater();
+        m_reply = nullptr;
+    }
 
-    // Parse for the data
+    QUrlQuery geoQuery;
 
+    geoQuery.addQueryItem("q", ApiAccess::getApiCityCountry());
+    geoQuery.addQueryItem("limit", QString::number(1)); // limit one place
+    geoQuery.addQueryItem("appid", ApiAccess::getApiKey());
+
+    m_reply = m_manager->get(QNetworkRequest(ApiAccess::GEOCODE_URL + "direct?" + geoQuery.toString()));
+
+    connect(m_reply, &QNetworkReply::finished, this, &WeatherApi::parseGeoData);
+
+    // parseGeoData NEEDS to return the latitude and longitude for the next API request.
+    // So find a way to do this with a simple yet correct way.
     // API request for weather info
 
     // Parse for the data
+}
+
+void WeatherApi::parseGeoData()
+{
+    if(m_reply->error() == QNetworkReply::NoError) {
+        QByteArray data = m_reply->readAll();
+
+        QJsonDocument jsonDocument = QJsonDocument::fromJson(data);
+
+        // finish the rest of the parsing
+    }
+    else
+        qWarning() << "Error from API reply: " + m_reply->errorString();
 }
 
 void WeatherApi::setInfo(const QString &data)
--- Weather-Service/WeatherApi.h
+++ Weather-Service/WeatherApi.h
@@ -1,8 +1,21 @@
 #ifndef WEATHERAPI_H
 #define WEATHERAPI_H
 
-#include <QNetworkAccessManager>
 #include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QProcessEnvironment>
+#include <QUrlQuery>
+#include <QNetworkRequest>
+#include <QJsonDocument>
+
+namespace ApiAccess {
+    inline const QString GEOCODE_URL = "http://api.openweathermap.org/geo/1.0/";
+    inline const QString WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+
+    inline const QString getApiKey();
+    inline const QString getApiCityCountry(); // City & Country(ISO 3166 code) divided by comma
+}
 
 class WeatherApi : public QObject
 {
@@ -16,6 +29,7 @@ public:
 
 public slots:
     void fetchData();
+    void parseGeoData();
 
 signals:
     void infoChanged();
@@ -23,7 +37,8 @@ signals:
 private:
     void setInfo(const QString &data);
 
-    QNetworkAccessManager *m_Manager;
+    QNetworkAccessManager *m_manager;
+    QNetworkReply *m_reply = nullptr;
     QString m_info;
 };
 
